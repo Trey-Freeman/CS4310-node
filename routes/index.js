2c0c2ffde876eebc3b34035fefaf8bdeb4840ee1
Steven <srjames90@berkeley.edu>
2016-09-30 17:42:07 -0700

    Added some comments in the routes. Added case for logged in user trying to access loging page. Made username and password required on front-end of both the registration page and the login page.

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,38 @@ var passport = require('passport');
 var Account = require('../models/account');
 var router = express.Router();
 
-
+/* GET Handle request to render te index page. If a user is logged in, dispay that user */
 router.get('/', function (req, res) {
     res.render('index', { user : req.user });
 });
 
+/* GET Handle request for registration page. Render registration page */
 router.get('/register', function(req, res) {
-    res.render('register', { });
+    res.render('register');
 });
 
+/* POST Handle a registration through post. If a username and password is provided, then set new account entry in mongo */
 router.post('/register', function(req, res) {
     Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
         if (err) {
             return res.render('register', { account : account });
         }
-
+        //Log user in and redirect to the root
         passport.authenticate('local')(req, res, function () {
             res.redirect('/');
         });
     });
 });
 
+/* GET handle request for the login page. If a user is already signed in, then redirect to root */
 router.get('/login', function(req, res) {
-    res.render('login', { user : req.user });
+    //If User is already logged in, then return to the index page
+    if(req.isAuthenticated())
+        return res.redirect('/');
+    res.render('login');
 });
 
+/* POST handle login request */
 router.post('/login', passport.authenticate('local'), function(req, res) {
     res.redirect('/');
 });
