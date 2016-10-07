31d550660ea8808026930476da135ea8bed4c543
Steven <srjames90@berkeley.edu>
2016-10-06 19:13:14 -0700

    Adding mongo path for heroku

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
 // mongoose
-mongoose.connect('mongodb://localhost/passport_local_mongoose_express4');
+mongoose.connect('mongodb://test:deploy@ds053186.mlab.com:53186/heroku_xvckjmjv');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
