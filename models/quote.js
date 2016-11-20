ca915176afe1fe3845a31094a70dbab2a20d283a
Trey Freeman <monkafur@gmail.com>
2016-11-19 21:47:41 -0800

    Added routes and fixed showQuotes to act as list. Added dropdown for quotes list and new quotes.

--- models/quote.js
+++ models/quote.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Quote = new Schema({
+   account      : String,
    id           : String,
    origin       : String,
    destination  : String,
--- routes/quote.js
+++ routes/quote.js
@@ -4,11 +4,11 @@ var Quote = require('../models/quote');
 var router = express.Router();
 const http = require('http');
 
-// router.all('/*', function(req, res, next) {
-//     if(!req.isAuthenticated())
-//         return res.redirect('/login');
-//     next();
-// });
+router.all('/*', function(req, res, next) {
+    if(!req.isAuthenticated())
+        return res.redirect('/login');
+    next();
+});
 
 //When someone types in localhost:3000/quote takes them to test.jade
 router.get('/', function(req, res) {
@@ -20,6 +20,7 @@ router.post('/po', function(req, res) {
     var quote = req.body;
     console.log(req.body);
     Quote.create({
+          account       : req.user._id,
            id           : body.id,
            origin       : body.origin,
            destination  : body.destination,
@@ -45,19 +46,20 @@ router.post('/make', function(req, res) {
     var request = require('request');
     var quote = req.body;
     var quote = new Quote({
-           id           : body.id,
-           origin       : quote.origin,
-           destination  : quote.destination,
-           price        : quote.price,
-           mpg          : quote.mpg,
-           distance     : quote.distance,
-           gasType      : quote.gasType,
-           ppm          : quote.ppm,
-           gallons      : quote.gallons,
-           gasCost      : quote.gasCost,
-           totalCost    : quote.totalCost,
-           ppmCost      : quote.ppmCost,
-           gasList      : body.gasList
+          account      : req.user._id,
+          id           : body.id,
+          origin       : quote.origin,
+          destination  : quote.destination,
+          price        : quote.price,
+          mpg          : quote.mpg,
+          distance     : quote.distance,
+          gasType      : quote.gasType,
+          ppm          : quote.ppm,
+          gallons      : quote.gallons,
+          gasCost      : quote.gasCost,
+          totalCost    : quote.totalCost,
+          ppmCost      : quote.ppmCost,
+          gasList      : body.gasList
         }, function(err) {
             if(err) return res.status(500).send(err);
             res.redirect('/');
@@ -74,6 +76,7 @@ router.post('/create', function(req, res) {
 
      var body       = req.body;
      var quote = new Quote({
+          account       : req.user._id,
            id           : body.id,
            origin       : body.origin,
            destination  : body.destination,
@@ -113,45 +116,51 @@ router.post('/create', function(req, res) {
        
       //JSON.stringify(tst);
       
-     request.post( 'http://maps.googleapis.com/maps/api/distancematrix/json?origins=' + body.origin + '&destinations=' + body.destination + '&units=imperial&mode=driving&language=en', function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-              var json = JSON.parse(body);
-              var distance = json.rows[0].elements[0].distance.value
-          } else {
-            return res.redirect('/quote')
-          }
+  request.post( 'http://maps.googleapis.com/maps/api/distancematrix/json?origins=' + body.origin + '&destinations=' + body.destination + '&units=imperial&mode=driving&language=en', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var json = JSON.parse(body);
+            var distance = json.rows[0].elements[0].distance.value
+        } else {
+          return res.redirect('/quote')
+        }
 
-          if (inputValue == "Submit") {
-            return res.render('quote', {user: req.user, quote: quote, distance: distance});
-          }
-          else if (inputValue == "/quote/make") {
-            return res.render('viewQuote', {user: req.user, quote: quote, distance: distance});
-          }
-          else if (inputValue == "/quote/print") {
+        if (inputValue == "Submit") {
+          return res.render('quote', {user: req.user, quote: quote, distance: distance});
+        }
+        else if (inputValue == "/quote/make") {
+          return res.render('viewQuote', {user: req.user, quote: quote, distance: distance});
+        }
+        else if (inputValue == "/quote/print") {
 
-            return res.render('printQuote', {user: req.user, quote: quote, distance: distance});
-          }
-          else if (inputValue == "/quote/po") {
-            //todo: save to db
+          return res.render('printQuote', {user: req.user, quote: quote, distance: distance});
+        }
+        else if (inputValue == "/quote/po") {
+          //todo: save to db
 
-             quote.save (function(err){
-                    if(err) return res.status(500).send(err);
-             });
-            Quote.count(function(err, quotes) {
+           quote.save (function(err){
+                  if(err) return res.status(500).send(err);
+           });
+          Quote.count(function(err, quotes) {
 
-              //console.log(quotes)
-            var count = 1 + Number(quotes)
-             //console.log(count);
-            return res.render('viewQuote', {user: req.user, quote: quote, distance: distance, count: count});
+            //console.log(quotes)
+          var count = 1 + Number(quotes)
+           //console.log(count);
+          return res.render('viewQuote', {user: req.user, quote: quote, distance: distance, count: count});
 
-            })
-            
-          }
-           else {
-            console.log('route error');
-          }            
-      });
+          })
+          
+        }
+         else {
+          console.log('route error');
+        }            
+  });
+});
 
+router.get('/list', function(req, res) {
+    Quote.find({account: req.user._id}, function(err, quotes) {
+        var username = req.user.username.charAt(0).toUpperCase() + req.user.username.substr(1).toLowerCase();
+        res.render('summaryQuote', {user: username, quotes: quotes});
+    });
 });
 
 
