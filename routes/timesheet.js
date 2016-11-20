89c5a485d1792e5972fa65c9d0e00603b611f3e9
Trey Freeman <monkafur@gmail.com>
2016-11-19 17:09:52 -0800

    Adding and fixing up the blurbs on the index and layout pages. Migrated php timesheet to jade timesheet.

--- routes/timesheet.js
+++ routes/timesheet.js
@@ -13,37 +13,11 @@ var router = express.Router();
 
 /* Test Code for Zaid's PHP timesheet */
 router.get('/', function(req, res){
-    exec("php ./timesheet/event_log.php", function (error, stdout, stderr) {
-        res.send(stdout);
-    });
+    res.render('timesheet');
 });
 
 router.post('/save', function(req, res){
-
-    var headers = {
-        'User-Agent':       'Super Agent/0.0.1',
-        'Content-Type':     'application/x-www-form-urlencoded'
-    }
-
-    // Configure the request
-    var options = {
-        url: 'http://localhost/timesheet/event_log.php',
-        method: 'POST',
-        headers: headers,
-        form:  req.body
-    }
-
-    // Start the request
-    request(options, function (error, response, body) {
-        console.log(error);
-        console.log(body);
-        if (!error && response.statusCode == 200) {
-            // Print out the response body
-            console.log(body)
-        }
-        res.send(body);
-
-    });    
+   
 });
 
 module.exports = router;
