926b9d938e24001fe8c04a258946d649680d27c5
Trey Freeman <monkafur@gmail.com>
2016-11-22 18:28:07 -0800

    Steven and I fixed this bug in the timesheets.

--- routes/timesheet.js
+++ routes/timesheet.js
@@ -30,7 +30,7 @@ router.post('/save', function(req, res) {
             if(err) return res.status(500).send(err);
     });
     console.log(timesheet);
-    res.render('timesheets');
+    res.redirect('/timesheet/list');
 });
 
 router.get('/list', function(req, res) {
