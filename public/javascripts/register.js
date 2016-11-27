0d93c510369acf311fd9908058caf16f9c24027e
Steven <srjames90@berkeley.edu>
2016-11-26 17:47:01 -0800

    Removing registration password validation. It doesn't work and makes the sites crash. Plus, there already an existing onclick that's meant to do validation (thought it doesn't check for special characters and etc.).

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -12,4 +12,21 @@
 	function verifyPassword(pass, passConf) {
 		return pass === passConf;
 	}
+
+	function validatePassword(fld, fld2) {
+      var error = "";
+      var illegalChars = /[\W_]/; // allow only letters and numbers
+   
+      if (fld.value != fld2.value) {
+          error = "Passwords must match . \n";
+          fld.style.background = 'Yellow';
+          fld2.style.background = 'Yellow';
+          alert(error);
+          return false;
+   
+      } else {
+          fld.style.background = 'White';
+      }
+     return true;
+    }
 })();
