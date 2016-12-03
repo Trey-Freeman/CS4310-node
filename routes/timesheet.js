911e625460df137c1d78e2cfd6c767ccee213404
chezwhite <contact@asblanco.com>
2016-12-03 14:30:22 -0800

    Edit timesheet

--- routes/timesheet.js
+++ routes/timesheet.js
@@ -53,6 +53,50 @@ router.get('/list', function(req, res) {
     
 });
 
+router.get('/edit/:timesheetId', function(req, res) {
+    Account.findOne({_id: req.user._id}, function(err, user) {
+        if(err) return res.status(500).send(err);
+        Timesheet.find({account: req.user._id}, function(err, timesheets) {
+            if(err) return res.status(500).send(err);
+            var username = req.user.username.charAt(0).toUpperCase() + req.user.username.substr(1).toLowerCase();
+            var timesheetId = req.params.timesheetId;
+            var n = req.query.n;
+            return res.render('edit_timesheet', {user: user, timesheets: timesheets, timesheetId: timesheetId, n: n});
+        });
+    })
+    
+});
+
+router.post('/edit/:timesheetId', function(req, res) {
+    var timesheet = req.body;
+
+    //Update it
+    Timesheet.update({_id: req.params.timesheetId}, {
+        startDate: timesheet.startDate,
+        endDate: timesheet.endDate,
+        hours: timesheet.hours,
+        rate: timesheet.rate,
+        comments: timesheet.comments
+    }, function (err) {
+      if (err) {
+          res.send("There was a problem updating the information to the database: " + err);
+      } 
+      else {
+              //HTML responds by going back to the page or you can be fancy and create a new view that shows a success page.
+              res.format({
+                  html: function(){
+                       res.redirect('/timesheet/list');
+                 },
+                 //JSON responds showing the updated values
+                json: function(){
+                       res.json(Timesheet);
+                 }
+              });
+       }
+    })
+
+});
+
 router.post('/delete', function(req, res) {
     var timesheetID = req.body.id
     Timesheet.remove({ _id: timesheetID }, function(err) {
